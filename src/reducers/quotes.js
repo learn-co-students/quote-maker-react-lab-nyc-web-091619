21472e2461bfad67d1f6cbc1dcc770d749937461
Altaf Quadri <altafmquadri@gmail.com>
2019-12-08 20:46:25 -0500

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,32 @@
 export default (state = [], action) => {
-  return state;
+  let idx
+  let quote
+  let newQuote
+
+  
+  switch(action.type){
+  
+    case "ADD_QUOTE":
+      return [ ...state, action.quote].map(state => Object.assign({}, state, {votes: 0})) 
+
+    case "REMOVE_QUOTE":
+      return [...state.filter(quote => quote.id !== action.quoteId )]
+
+    case "UPVOTE_QUOTE":
+      idx = state.findIndex(quote => quote.id === action.quoteId)
+      quote = state[idx]
+      newQuote = {...quote}
+      newQuote.votes += 1
+      return [...state.slice(0, idx), newQuote, ...state.slice(idx + 1)]
+
+    case "DOWNVOTE_QUOTE":
+      idx = state.findIndex(quote => quote.id === action.quoteId)
+      quote = state[idx]
+      newQuote = {...quote}
+      newQuote.votes === 0 ? newQuote.votes = 0 : newQuote.votes -= 1
+      return [ ...state.slice(0, idx), newQuote, ...state.slice(idx + 1)]
+      
+    default:
+      return state;
+  }
 }
