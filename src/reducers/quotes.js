b7d480ee556de460cca71243560ab2095356fa9c
maryfarner18 <maryfarner@gmail.com>
2019-12-10 17:01:51 -0500

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,23 @@
 export default (state = [], action) => {
-  return state;
+  let idx
+  switch (action.type){
+    case "ADD_QUOTE":
+      return [...state, action.quote]
+
+    case "REMOVE_QUOTE":
+      return state.filter(quote => quote.id !== action.quoteId)
+
+    case "UPVOTE_QUOTE":
+      idx = state.findIndex(quote => quote.id === action.quoteId)
+      let newQuote = {...state[idx], votes: (state[idx].votes + 1)}
+      return [...state.slice(0, idx), newQuote, ...state.slice(idx + 1)]
+
+    case "DOWNVOTE_QUOTE":
+      idx = state.findIndex(quote => quote.id === action.quoteId)
+      newQuote = {...state[idx], votes: Math.max(0, (state[idx].votes - 1))}
+      return [...state.slice(0, idx), newQuote, ...state.slice(idx + 1)]
+    
+    default:
+      return state;
+  }
 }
