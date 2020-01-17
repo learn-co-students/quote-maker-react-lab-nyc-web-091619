3737caf7fdb830f7617d0ff26a25f2cb4407732b
Danny Lee <dreamer@panix.com>
2020-01-17 10:59:13 -0500

    add reducers and components to index.js

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,28 @@
 // TODO: Create action creators as defined in tests
+export const addQuote = quote => {
+  return {
+    type: 'ADD_QUOTE',
+    quote: Object.assign({}, quote, { votes: 0 })
+  }
+}
+
+export const removeQuote = quoteID => {
+  return {
+    type: 'REMOVE_QUOTE',
+    quoteID
+  }
+}
+
+export const upVoteQuote = quoteID => {
+  return {
+    type: 'UPVOTE_QUOTE',
+    quoteID
+  }
+}
+
+export const downVoteQuote = quoteID => {
+  return {
+    type: 'DOWNVOTE_QUOTE',
+    quoteID
+  }
+}
